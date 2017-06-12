841a087921adfbabad5bb5fd57a23995f587c521
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-06-11 23:17:44 -0300

    Ajustando pastas de css e javascript e incluindo bibliotoca do material design

--- ClinicaPilotagemWeb/App_Start/BundleConfig.cs
+++ ClinicaPilotagemWeb/App_Start/BundleConfig.cs
@@ -9,10 +9,10 @@ namespace ClinicaPilotagemWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -20,11 +20,11 @@ namespace ClinicaPilotagemWeb
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap/bootstrap.js",
+                      "~/Scripts/bootstrap/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
